2de94617d3bc65921e6f828b51b7a03c840082c1
Martin Schnabel <martin.schnabel@bridgefield.de>
2025-09-26 21:10:46 +0200

    feat(client): reject positional arguments with helpful error message

--- client/main.go
+++ client/main.go
@@ -1,59 +1,65 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
+	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
-    var share string
-    var mountpoint string
-    var addr string
-    var readOnly bool
+	var share string
+	var mountpoint string
+	var addr string
+	var readOnly bool
 
-    flag.StringVar(&share, "share", "", "Share name or path exposed by server")
-    flag.StringVar(&mountpoint, "mountpoint", "", "Mount point (Linux)")
-    flag.StringVar(&addr, "addr", "127.0.0.1:50051", "server address")
-    flag.BoolVar(&readOnly, "ro", true, "mount read-only")
-    
-    flag.Usage = func() {
-        fmt.Fprintf(os.Stderr, "Usage: %s [options]\n\n", os.Args[0])
-        fmt.Fprintf(os.Stderr, "Options:\n")
-        flag.PrintDefaults()
-        fmt.Fprintf(os.Stderr, "\nExamples:\n")
-        fmt.Fprintf(os.Stderr, "  %s --share test --mountpoint /mnt/fsdriver/test\n", os.Args[0])
-        fmt.Fprintf(os.Stderr, "  %s --share test --mountpoint /mnt/fsdriver/test --addr 127.0.0.1:50055\n", os.Args[0])
-    }
-    
+	flag.StringVar(&share, "share", "", "Share name or path exposed by server")
+	flag.StringVar(&mountpoint, "mountpoint", "", "Mount point (Linux)")
+	flag.StringVar(&addr, "addr", "127.0.0.1:50051", "server address")
+	flag.BoolVar(&readOnly, "ro", true, "mount read-only")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options]\n\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "\nExamples:\n")
+		fmt.Fprintf(os.Stderr, "  %s --share test --mountpoint /mnt/fsdriver/test\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s --share test --mountpoint /mnt/fsdriver/test --addr 127.0.0.1:50055\n", os.Args[0])
+	}
+
     flag.Parse()
 
+    // Check for positional arguments (not allowed)
+    if len(flag.Args()) > 0 {
+        fmt.Fprintf(os.Stderr, "Error: positional arguments not allowed: %v\n", flag.Args())
+        fmt.Fprintf(os.Stderr, "Use --addr flag instead: --addr %s\n", flag.Args()[0])
+        flag.Usage()
+        os.Exit(2)
+    }
+
     // Validate required parameters
-    if share == "" {
-        fmt.Fprintln(os.Stderr, "Error: --share is required")
-        flag.Usage()
-        os.Exit(2)
-    }
-    
-    if mountpoint == "" {
-        fmt.Fprintln(os.Stderr, "Error: --mountpoint is required")
-        flag.Usage()
-        os.Exit(2)
-    }
-    
-    // Validate mountpoint exists and is a directory
-    if info, err := os.Stat(mountpoint); err != nil {
-        fmt.Fprintf(os.Stderr, "Error: mountpoint '%s' does not exist: %v\n", mountpoint, err)
-        os.Exit(2)
-    } else if !info.IsDir() {
-        fmt.Fprintf(os.Stderr, "Error: mountpoint '%s' is not a directory\n", mountpoint)
-        os.Exit(2)
-    }
+	if share == "" {
+		fmt.Fprintln(os.Stderr, "Error: --share is required")
+		flag.Usage()
+		os.Exit(2)
+	}
 
-    if err := mountRemote(share, mountpoint, addr, readOnly); err != nil {
-        fmt.Fprintf(os.Stderr, "Mount error: %v\n", err)
-        os.Exit(1)
-    }
+	if mountpoint == "" {
+		fmt.Fprintln(os.Stderr, "Error: --mountpoint is required")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	// Validate mountpoint exists and is a directory
+	if info, err := os.Stat(mountpoint); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: mountpoint '%s' does not exist: %v\n", mountpoint, err)
+		os.Exit(2)
+	} else if !info.IsDir() {
+		fmt.Fprintf(os.Stderr, "Error: mountpoint '%s' is not a directory\n", mountpoint)
+		os.Exit(2)
+	}
+
+	if err := mountRemote(share, mountpoint, addr, readOnly); err != nil {
+		fmt.Fprintf(os.Stderr, "Mount error: %v\n", err)
+		os.Exit(1)
+	}
 }
-
-
